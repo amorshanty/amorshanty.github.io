419cd77eb48fb59ca304177da2a6ce61dfa06f5a
Amor <amor.dibella@gmail.com>
2015-01-21 17:07:09 +0100

    l

--- Bootcamp/semana_2/sl7.rb
+++ Bootcamp/semana_2/sl7.rb
@@ -3,10 +3,14 @@
 # OK, so we got ourselves comfortable with Sinatra, and the way he plays the songs we write. That’s nice, our first web applications!
 # Remember this moment :)
 
-# Now I want you to think on a small web application with no UX at all and just a bit of interaction with a web client. You can do iterations
-# on it so you add one feature at a time.
+# Now I want you to think on a small web application with no UX at all and just a bit of interaction with a web client. You can do iterations on it so you add one feature at a time.
 
-# Take a moment to read a bit of Sinatra documentation (test yourself!) and play with this tiny web framework. Use several routes, redirects,
-# parameters and conditions.
+# Take a moment to read a bit of Sinatra documentation (test yourself!) and play with this tiny web framework. Use several routes, redirects, parameters and conditions.
 
 # Go ahead! :D
+
+require 'sinatra'
+require 'sinatra/reloader'
+
+set :port, 3000
+set :bind, '0.0.0.0'
