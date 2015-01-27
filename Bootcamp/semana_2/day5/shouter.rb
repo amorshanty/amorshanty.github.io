d389123501ec1f104e0eb0a681b28b6b151d4bab
Amor <amor.dibella@gmail.com>
2015-01-27 11:17:29 +0100

    1

--- Bootcamp/semana_2/day5/shouter.rb
+++ Bootcamp/semana_2/day5/shouter.rb
@@ -5,6 +5,7 @@ require 'sinatra/reloader'
 
 set :port, 3000
 set :bind, '0.0.0.0'
+enable :sessions
 
 ActiveRecord::Base.establish_connection(
   adapter: 'sqlite3',
@@ -12,17 +13,15 @@ ActiveRecord::Base.establish_connection(
 )
 
 class User < ActiveRecord::Base
-	validates_presence_of :name, :password
+	validates_presence_of :name, :password, length: { minimum: 5 }
 	validates_format_of :handle, with: /\A@/ 
 	has_many :shout
-
 end
 
 class Shout < ActiveRecord::Base
-
-	validates_presence_of :message, :user, :created_at
+	validates_presence_of :message, :user
 	validates :message, length: { in: 1..200 }
-	belongs_to :users
+	belongs_to :user
 end
 
 
@@ -30,18 +29,48 @@ id=0
 
 get '/' do
 	@users_list = User.all
+	@shout = Shout.all 
 	@id = id 
   erb :user
 end
 
-post '/newuser' do
+get 'logout' do
+	session.clear
+	redirect '/'
+end
 
+
+post '/newuser' do
 	id += 1
   newuser = User.new
-  newuser.id = id
   newuser.name = params[:name]
   newuser.handle = params[:handle]
   newuser.password = params[:password]
-  newuser.save
-  redirect '/'
+  
+  if newuser.save
+  	session[:user_id] = newuser.id
+  	redirect '/shouting'
+  else
+  	redirect("/")
+  end
+
 end
+
+get '/shouting' do
+	@user = User.find(session[:user_id])
+	@shouts = Shout.all
+  erb :shout
+end
+
+post '/newshout' do
+	newshout = Shout.new
+	newshout.message = params[:message]
+	newshout.user = User.find(session[:user_id])
+	newshout.created_at = Date.today
+	newshout.save
+	redirect '/shouting'
+
+end
+
+__END__
+
